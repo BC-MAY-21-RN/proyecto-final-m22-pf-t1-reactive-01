58147e2918b7a33253b3bb2aedb818e156c9604f
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-07-11 21:59:57 -0300

    add project structure

--- MyPets/App.js
+++ MyPets/App.js
@@ -4,7 +4,7 @@ import React, {useEffect} from 'react';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <Text>App Text</Text>
     </View>
   );
 };
