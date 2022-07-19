8962a6fd5956bde7c8a5bc1c1680b2d1e29cd65e
Ayeza Elizabeth Zamudio Comino <ayezahmvc14@gmail.com>
2022-07-19 16:37:04 -0500

    Create slice

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,8 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import Layout from '../../layout/Layout';
-
+import {store} from '../../store/store';
+import {setemail, setpassword} from '../../store/slices/users/userSlice';
 const HomeScreen = () => {
+  console.log(store.getState().userinformation.email);
+  store.dispatch(setemail('Ayeza'));
   return (
     <Layout>
       <Text>HomeScreen</Text>
