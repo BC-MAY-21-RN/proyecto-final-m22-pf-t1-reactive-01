a83c9a01a5c6ea3c85fd3975c8f6fb940952189a
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-07-22 22:42:50 -0300

    add route sign up

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,4 +3,5 @@ export {default as Login} from './LoginScreen/LoginScreen';
 export {default as Onboarding} from './OnBoardingScreen/OnBoardingScreen';
 export {default as Home} from './Home/HomeScreen';
 export {default as ResetPassword} from './ResetPasswordScreen/ResetPasswordScreen';
+export {default as SignInScreen} from './SingInScreen/SignInScreen';
 export {default as HomeClient} from './Client/Home/HomeScreen';
