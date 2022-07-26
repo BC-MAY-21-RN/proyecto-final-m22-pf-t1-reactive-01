26b345508d81c5283ad40102955442442b56d4c7
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-07-26 11:08:42 -0300

    add icon and button

--- src/screens/Client/Upload/UploadPets.js
+++ src/screens/Client/Upload/UploadPets.js
@@ -1,11 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Layout from '../../../layout/Layout';
+import Upload from '../../../assets/svg/upload.svg';
+import CustomButton from '../../../components/Button/CustomButton';
 const UploadPets = () => {
   return (
     <Layout>
       <View style={styles.container}>
-        <Text>UploadPets</Text>
+        <View style={styles.containerIcon}>
+          <Upload widht={200} height={200} />
+          <Text style={styles.title}>Take Picture</Text>
+        </View>
+        <View style={styles.containerButton}>
+          <CustomButton title="Next" />
+        </View>
       </View>
     </Layout>
   );
@@ -16,8 +24,21 @@ export default UploadPets;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  containerIcon: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    textAlign: 'center',
+    alignContent: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  containerButton: {
+    flex: 1,
+    justifyContent: 'space-around',
   },
 });
