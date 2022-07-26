1c0f1bd61ad60feb9cbd37da26b9f6ce617cb811
FranciscoSenestrari <fran.senes@gmail.com>
2022-07-26 00:11:44 -0300

    styling buttons and create walker menu

--- src/components/Button/SocialButton.js
+++ src/components/Button/SocialButton.js
@@ -1,11 +1,29 @@
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import FacebookIcon from '../../assets/svg/facebook.svg';
 import GoogleIcon from '../../assets/svg/google.svg';
+import theme from '../../assets/theme/theme';
 
 const SocialButton = ({type, size}) => {
+  const styles = StyleSheet.create({
+    button: {
+      margin: 5,
+      width: size,
+      height: size,
+      borderRadius: size / 2,
+      backgroundColor: theme.BACKGROUND_COLOR,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62,
+      elevation: 4,
+    },
+  });
   return (
-    <TouchableOpacity style={{margin: 5, width: size, height: size}}>
+    <TouchableOpacity style={styles.button}>
       {type === 'facebook' ? (
         <FacebookIcon widht={size} height={size} />
       ) : type === 'google' ? (
