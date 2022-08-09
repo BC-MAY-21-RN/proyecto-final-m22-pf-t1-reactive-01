4b5190154f85009ebb211796e0859d7b80022823
FranciscoSenestrari <fran.senes@gmail.com>
2022-08-08 23:54:46 -0300

    style on external file

--- src/components/InputPetInfo/Input.js
+++ src/components/InputPetInfo/Input.js
@@ -1,8 +1,7 @@
-import {StyleSheet, TextInput, View} from 'react-native';
+import {TextInput, View} from 'react-native';
 import React, {useState} from 'react';
 import SelectDropdown from 'react-native-select-dropdown';
-import theme from '../../assets/theme/theme';
-
+import {styles} from './style';
 const Input = ({type, onChangeText, placeholder}) => {
   const petArray = ['Dog', 'Cat'];
   const breedArray = [
@@ -128,36 +127,3 @@ const Input = ({type, onChangeText, placeholder}) => {
 };
 
 export default Input;
-
-const styles = StyleSheet.create({
-  input: {
-    fontFamily: 'OpenSans-Medium',
-    margin: 10,
-    backgroundColor: '#ededed',
-  },
-  inputshort: {
-    fontFamily: 'OpenSans-Medium',
-    margin: 10,
-    backgroundColor: '#ededed',
-    width: theme.WIDTH_SCREEN / 1.5,
-  },
-  inputmultiline: {
-    backgroundColor: '#ededed',
-    height: 100,
-    maxHeight: 100,
-  },
-  inputgroup: {
-    fontFamily: 'OpenSans-Medium',
-    margin: 10,
-    backgroundColor: '#ededed',
-    width: theme.WIDTH_SCREEN / 2.3,
-  },
-  dropdown: {
-    margin: 10,
-    width: theme.WIDTH_SCREEN / 2.3,
-  },
-  rowStyle: {
-    width: 150,
-    backgroundColor: '#F6F6F6',
-  },
-});
