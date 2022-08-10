9d8ce77d0078fbb213ab3288756ffce1710f8b47
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-08-10 00:51:28 -0300

    modify title

--- src/screens/Client/Geolocalization/GeolocationTest.js
+++ src/screens/Client/Geolocalization/GeolocationTest.js
@@ -1,6 +1,8 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useState} from 'react';
 import MapView, {Marker, Polyline} from 'react-native-maps';
+import NearItem from '../../../components/NearItem/NearItem';
+import {ScrollView} from 'react-native-gesture-handler';
 const GeolocationTest = () => {
   const [origen, setOrigen] = useState({
     latitud: -37.970669,
@@ -26,6 +28,19 @@ const GeolocationTest = () => {
           <Marker coordinate={{latitude: -37.970237, longitude: -57.584121}} />
         </MapView>
       </View>
+      <View style={styles.containerCard}>
+        <Text style={styles.title}>Nearby Walkersr</Text>
+        <ScrollView>
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+          <NearItem name="Francisco Jose " hour="16:30" distance="1.3 KM" />
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -34,9 +49,11 @@ export default GeolocationTest;
 
 const styles = StyleSheet.create({
   container: {flex: 1, margin: 10},
-  containerMap: {flex: 0.4},
+  containerMap: {flex: 0.5},
   map: {
     width: '100%',
     height: '100%',
   },
+  containerCard: {flex: 0.5},
+  title: {fontSize: 25},
 });
