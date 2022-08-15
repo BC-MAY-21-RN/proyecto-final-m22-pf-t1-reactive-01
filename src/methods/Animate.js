dcbe24c2a0db47522550d9bd81542cc6cde01a62
FranciscoSenestrari <fran.senes@gmail.com>
2022-08-15 20:24:12 -0300

    modify animation

--- src/methods/Animate.js
+++ src/methods/Animate.js
@@ -1,27 +1,23 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
+import {Animated, Easing} from 'react-native';
 import Lottie from 'lottie-react-native';
-import {View} from 'react-native';
 
-export default function Animate(type) {
-  const animationRef = useRef < Lottie > null;
+export default function Animate() {
+  const animationProgress = React.useRef(new Animated.Value(0));
 
   useEffect(() => {
-    animationRef.current?.play();
+    Animated.timing(animationProgress.current, {
+      toValue: 1,
+      duration: 5000,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }).start();
   }, []);
 
   return (
-    <>
-      {type === 'login' ? (
-        <Lottie
-          ref={animationRef}
-          source={require('../assets/animations/login-an.json')}
-        />
-      ) : (
-        <Lottie
-          ref={animationRef}
-          source={require('../assets/animations/missing-an.json')}
-        />
-      )}
-    </>
+    <Lottie
+      source={require('../assets/animations/login-an.json')}
+      progress={animationProgress.current}
+    />
   );
 }
