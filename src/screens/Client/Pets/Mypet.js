85a515566e0a0b9cf74ed7f7fcc89665852ef5ec
FranciscoSenestrari <fran.senes@gmail.com>
2022-08-23 17:09:40 -0300

    fix bugs

--- src/screens/Client/Pets/Mypet.js
+++ src/screens/Client/Pets/Mypet.js
@@ -6,39 +6,7 @@ import Card from './CardPet';
 import {styles} from './styles';
 import ButtonAdd from '../../../components/Button/ButtonAdd';
 import {retrivePets} from '../../../methods/SavePet';
-const data = [
-  {
-    id: 1,
-    namePet: 'cucu ',
-    image:
-      'https://cdn.mequieroir.com/wp-content/uploads/2019/10/mascota-espana-blanca-1024x639.jpg',
-    gender: 'Male',
-    color: 'rojo ',
-    age: '2',
-    description: 'he have very long hair and red color ',
-  },
-  {
-    id: 2,
-    namePet: 'pepe ',
-    image:
-      'https://cdn.mequieroir.com/wp-content/uploads/2019/10/mascota-espana-blanca-1024x639.jpg',
-    gender: 'Male',
-    color: 'blanco ',
-    age: '2',
-    description: 'he not have tail',
-  },
-  {
-    id: 3,
-    namePet: 'francisco ',
-    image:
-      'https://cdn.mequieroir.com/wp-content/uploads/2019/10/mascota-espana-blanca-1024x639.jpg',
-    gender: 'Male',
-    color: 'negro ',
-    age: '2',
-    description:
-      'It is a quite friendly and calm dog. It likes to take naps and responds to the call of "hotdog"',
-  },
-];
+
 const Mypet = ({navigation}) => {
   const [pets, setPets] = useState([]);
 
